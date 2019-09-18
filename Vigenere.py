38c4f6138773f8603a2ea8fbb35df238b150eee0
PromyseWard <55416601+PromyseWard@users.noreply.github.com>
2019-09-18 15:02:11 -0500

    Added Vigenere Cipher
    Base code is written. Can handle symbols/numerical characters and keys with spaces. Still need to implement stdin/stdout.

---
+++ Vigenere.py
@@ -0,0 +1,157 @@
+# Written in Python 3.7.
+
+import sys
+import enum
+import string
+
+# encrypts pain text to cipher text
+def Cipher(plainText, key):
+    P = list(plainText)
+    K = list(key)
+    C = [] * len(plainText)
+    x = 0
+    
+    # iterate through each index in the plain text
+    for i in range(0, len(P)):
+    
+        # x is used to track and repeat key
+        # when x reaches the key's length, it resets
+        if (x == len(K)):
+            x = 0
+            
+        # checks if the letter of the plain text is an alphabetic character
+        # if true: find and add integer value
+        if (P[i].isalpha()):
+            Pi = ASCIIValue(P[i])
+            Ki = ASCIIValue(K[x])
+            c_int = (Pi + Ki)%26
+            x+= 1
+            
+        # else: add symbol/numerical value
+        else:
+            c_int = P[i]
+        C.append(c_int)
+    return C
+    
+# decrypts cipher text to plain text
+def Decipher(cipherText, key):
+    C = list(cipherText)
+    K = list(key)
+    P = [] * len(cipherText)
+    x = 0
+
+    # iterate through each index in the cipher text
+    for i in range(0, len(C)):
+        
+        # x is used to track and repeat key
+        # when x reaches the key's length, it resets
+        if (x == len(K)):
+            x = 0
+            
+        # checks if the letter of the cipher text is an alphabetic character
+        # if true: find and add integer value     
+        if (C[i].isalpha()):
+            Ci = ASCIIValue(C[i])
+            Ki = ASCIIValue(K[x])
+            p_int = (Ci - Ki + 26)%26
+            x += 1
+            
+        # else: add symbol/nplainTextListerical value
+        else:
+            p_int = C[i]
+        P.append(p_int)
+    return P
+
+# returns the number value of a letter
+def ASCIIValue(letter):
+
+    # if the letter is lowercase, get lowercase alphabet
+    if letter.islower():
+        alphabet = list(string.ascii_lowercase)
+        
+    # else, get uppercase alphabet
+    else:
+        alphabet = list(string.ascii_uppercase)
+    
+    # find letter in alphabet
+    for i in range(len(alphabet)):
+        if (alphabet[i] == letter):
+            return i
+        
+# returns the letter value of a number
+def CharValue(int, case):
+
+    # if the letter was once uppercase, get lowercase alphabet
+    if case == True:
+        alphabet = list(string.ascii_lowercase)
+        
+    # else, get uppercase alphabet
+    else:
+        alphabet = list(string.ascii_uppercase)
+        
+    # return letter in alphabet
+    return alphabet[int]
+
+# MAIN CODE #
+
+######## TODO ###################################
+# Need to have way to: 
+# accept input from user (current way)
+# AND 
+# accept file from command line (statement below)
+##################################################
+
+#f = sys.stdin.read()
+
+# remove spaces from key (can also be done in cipher/decipher functions)
+key = sys.argv[2]
+key = key.replace(" ", "")
+
+# if want to encrypt a plain text
+if (sys.argv[1] == "-e"):
+    
+    while(True):
+        cipherText = ""
+        plainText = input()
+        plainTextList = list(plainText)
+        
+        # encipher plain text into a list of integers
+        intList = Cipher(plainText, key)
+        
+        
+        # convert each integer into it's mapped letter of the alphabet
+        for i in range(0, len(intList)):
+        
+            # check if the object is an integer
+            # if so, convert to letter
+            if(isinstance(intList[i], int)):
+                cipherText += str(CharValue(intList[i], plainTextList[i].islower()))
+            
+            # else, add symbol/numerical value
+            else: 
+                cipherText += intList[i]
+        print(cipherText)
+      
+# if want to decrypt a cipher text      
+elif (sys.argv[1] == "-d"):
+    while(True):
+        plainText = ""
+        cipherText = input()
+        plainTextList = list(cipherText)
+        
+        # decipher cipher text into a list of integers
+        intList = Decipher(cipherText, key)
+        
+        # convert each integer into it's mapped letter of the alphabet
+        for i in range(0, len(intList)):
+        
+            # check if the object is an integer
+            # if so, convert to letter
+            if(isinstance(intList[i], int)):
+                plainText += str(CharValue(intList[i], plainTextList[i].islower()))
+            
+            # else, add symbol/numerical value
+            else:
+                plainText += intList[i]
+        print(plainText)
+        
